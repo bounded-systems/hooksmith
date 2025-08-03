03c7c09028d3682b3562ed761e61903d86f252a5
Robert DeLanghe <1240090+bdelanghe@users.noreply.github.com>
2025-08-03 13:19:15 -0400

    chore: auto-update at 2025-08-03 17:19:15

--- test_validation_error.rs
+++ test_validation_error.rs
@@ -1,4 +1,5 @@
 fn main() {
     let unused_variable = 42; // This will trigger a Clippy warning
     println!("Hello, world!");
+    println!("Testing file watch functionality!"); // New line to trigger file watching
 } 
